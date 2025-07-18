499fe9c8889a86999b9e01c679eff9ed62877ae4
tonooga <tono.oga@gmail.com>
2025-07-18 16:06:41 +0200

    third attempt from scratch

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import About from '../components/About'
+
+const AboutPage = () => {
+  return (
+    <>
+        <About />
+    </>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+
+const ServicesPage = () => {
+  return (
+    <>
+        <Services />
+    </>
+  )
+}
+
+export default ServicesPage
